53dd0664929106f08bb0c9ec12fb0542166474e8
unknown <manofsteel1544@gmail.com>
2025-07-22 11:08:23 +0300

    update

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,7 +1,21 @@
+import Link from "next/link";
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="bg-white shadow-md py-4 px-8 flex items-center justify-between">
+      <h1 className="text-xl font-bold">My App</h1>
+
+      <nav className="space-x-4">
+        <Link href="/home" className="text-gray-700 hover:text-blue-500">
+          Home
+        </Link>
+        <Link href="/about" className="text-gray-700 hover:text-blue-500">
+          About
+        </Link>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
